68650131f286954a377db9d13fb23874a7e0acb6
manasroutcse <manasroutcse@gmail.com>
2024-09-24 00:41:51 +0530

    My second commit with changes in auth file

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,43 +15,43 @@
 
 
 // pages/api/auth/[...nextauth].js
-import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+// import NextAuth from "next-auth";
+// import Providers from "next-auth/providers";
 
-export default NextAuth({
-  providers: [
-    Providers.Credentials({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      authorize: async (credentials) => {
-        const { email, password } = credentials as any;
-        // Here is where you can validate the email/password and make the appropriate calls to a database or external service.
-        const user = await fetch("your-api-endpoint/authenticate", {
-          method: "POST",
-          body: JSON.stringify({ email, password }),
-          headers: { "Content-Type": "application/json" },
-        });
+// export default NextAuth({
+//   providers: [
+//     Providers.Credentials({
+//       name: "Credentials",
+//       credentials: {
+//         email: { label: "Email", type: "email" },
+//         password: { label: "Password", type: "password" },
+//       },
+//       authorize: async (credentials) => {
+//         const { email, password } = credentials as string;
+//         // Here is where you can validate the email/password and make the appropriate calls to a database or external service.
+//         const user = await fetch("your-api-endpoint/authenticate", {
+//           method: "POST",
+//           body: JSON.stringify({ email, password }),
+//           headers: { "Content-Type": "application/json" },
+//         });
 
-        const userInfo = await user.json();
+//         const userInfo = await user.json();
 
-        if (user.ok && userInfo) {
-          return userInfo;
-        }
+//         if (user.ok && userInfo) {
+//           return userInfo;
+//         }
 
-        return null;
-      },
-    }),
-  ],
-  callbacks: {
-    jwt: async (token, user) => {
-      if (user) {
-        token.id = user.id;
-      }
+//         return null;
+//       },
+//     }),
+//   ],
+//   callbacks: {
+//     jwt: async (token, user) => {
+//       if (user) {
+//         token.id = user.id;
+//       }
 
-      return token;
-    },
-  },
-});
+//       return token;
+//     },
+//   },
+// });
